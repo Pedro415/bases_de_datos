aae276c319a1e090577b2b1bf8e217fff3ce0010
tejcamilo <57298465+tejcamilo@users.noreply.github.com>
2021-05-24 00:18:05 -0500

    Entrega 3

--- entrega2/modulos.py
+++ entrega2/modulos.py
@@ -1,13 +1,57 @@
 
+def consulta1():
+    return "select llamada.anio, llamada.mes, loc.nombre, sum(llamada.acumulado_incidentes) as llamadas_recibidas" \
+            " from llamada join upz on (llamada.id_upz = upz.id) join localidad loc on (upz.id_localidad = loc.id)" \
+            " where anio = 2020 group by llamada.anio, llamada.mes, loc.nombre" \
+            " order by mes asc, llamadas_recibidas desc"
+
+def consulta2():
+    return "(select llam.anio as anio, inc.tipo as incidente, sum(llam.acumulado_incidentes) as total " \
+            "from incidente inc join llamada llam on (inc.codigo = llam.codigo_incidente) " \
+            "where llam.anio = 2020 and inc.tipo != '-' group by anio, incidente order by total desc)"
+
+def consulta3():
+    return "select *" \
+            " from ((select inc.tipo as incidente, sum(llam.acumulado_incidentes) as total_2020, llam.anio " \
+            "from incidente inc join llamada llam on (inc.codigo = llam.codigo_incidente) " \
+            "where llam.anio = 2020 and tipo != '-' group by anio, incidente order by incidente asc) " \
+            "union all" \
+            " (select inc.tipo as incidente, sum(llam.acumulado_incidentes) as total_2019, llam.anio" \
+            " from incidente inc join llamada llam on (inc.codigo = llam.codigo_incidente)" \
+            " where llam.anio = 2019 and tipo != '-' group by anio, incidente order by incidente asc)) as t2" \
+            " order by incidente, anio"
+
+def consulta4():
+    return "select upz.nombre, sum(llam.acumulado_incidentes) as total " \
+            "from upz join llamada llam on (upz.id = llam.id_upz) " \
+            "join incidente inc on (llam.codigo_incidente = inc.codigo) " \
+            "where upz.nombre!='SIN LOCALIZACION' and " \
+            "inc.tipo = 'ATRACO / HURTO EN PROCESO' or inc.tipo = 'HURTO EFECTUADO' " \
+            "group by upz.nombre order by total desc"
+
 import psycopg2
 
+class Connection:
+
+    def __init__(self):
+        self.connection = None
+
+    def openConnection(self):
+        try:
+            self.connection = psycopg2.connect(host="localhost",port="5432",dbname="proyecto_bd",user="postgres",
+                                               password="postgres")
+        except Exception as e:
+            print(e)
+
+    def closeConnection(self):
+        self.connection.close()
+"""
 con = psycopg2.connect(host="localhost",port="5432",dbname="proyecto_bd",user="postgres",password="postgres")
 print("Database opened successfully")
 
 cur = con.cursor()
 
-query = " select llamada.anio, llamada.mes, loc.nombre, sum(llamada.acumulado_incidentes) as llamadas_recibidas from llamada join upz on (llamada.id_upz = upz.id) join localidad loc on (upz.id_localidad = loc.id) where anio = 2020 group by llamada.anio, llamada.mes, loc.nombre order by mes asc, llamadas_recibidas desc "
-cur.execute(query)
+cur.execute(consulta1)
 rows = cur.fetchall()
 
 for row in rows:
@@ -19,3 +63,4 @@ for row in rows:
 
 print("Operation done successfully")
 con.close()
+"""
---
+++ entrega3/dashboard.py
@@ -0,0 +1,57 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import plotly.express as px
+import pandas as pd
+import modulos
+from modulos import Connection
+
+external_stylesheets = ["https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css"]
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+
+#Cases by country
+con = Connection()
+con.openConnection()
+consulta1 = pd.read_sql_query(modulos.consulta1(), con.connection)
+consulta2 = pd.read_sql_query(modulos.consulta2(), con.connection)
+consulta3 = pd.read_sql_query(modulos.consulta3(), con.connection)
+consulta4 = pd.read_sql_query(modulos.consulta4(), con.connection)
+con.closeConnection()
+consulta1 = pd.DataFrame(consulta1, columns=["anio", "mes", "nombre", "llamadas_recibidas"])
+consulta2 = pd.DataFrame(consulta2, columns=["incidente", "total"])
+consulta3 = pd.DataFrame(consulta3, columns=["mes", "total"])
+consulta4 = pd.DataFrame(consulta4, columns=["nombre","total"])
+figConsulta1 = px.bar(consulta1.head(20), x="nombre", y="llamadas_recibidas")
+figConsulta2 = px.pie(consulta2.head(20), values='total', names='incidente')
+figConsulta3 = px.bar(consulta3.head(20), x="mes", y="total")
+figConsulta4 = px.pie(consulta4.head(20), values="total", names="nombre")
+
+#Layout
+app.layout = html.Div(children=[
+    html.H1(children="Llamadas realizadas al 123", className="text-center"),
+    html.H2(children="Llamadas recibidas por localidad durante el año 2020", className="text-center"),
+    dcc.Graph(
+        id="consulta1",
+        figure=figConsulta1
+    ),
+    html.H2(children="Tipos de llamada más comunes durante el año 2020", className="text-center"),
+    dcc.Graph(
+        id="consulta2",
+        figure=figConsulta2
+    ),
+    html.H2(children="Flujo de llamadas por mes", className="text-center"),
+    dcc.Graph(
+        id="consulta3",
+        figure=figConsulta3
+    ),
+    html.H2(children="UPZs con la mayor cantidad de hurtos reportados en los últimos 5 años", className="text-center"),
+    dcc.Graph(
+        id="consulta4",
+        figure=figConsulta4
+    ),
+])
+
+if __name__ == "__main__":
+   app.run_server(debug=True)
